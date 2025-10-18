ffc3de407f1ab714539760ccba5b5dd6929399b2
Haidar <haidor.ch@gmail.com>
2025-10-18 23:17:33 +0200

    changes made to Layout

--- app.py
+++ app.py
@@ -20,3 +20,12 @@ col1, col2 = st.columns([1,1])
 selected_port = col1.selectbox(options=embarked_port, label="Select a Port")
 selected_gender = col2.selectbox(options=geneder, label="Select a Gender")
 
+#filter data based on selection
+df_plot = df[(df['Embarked'] == selected_port)]
+df_plot = df_plot[(df_plot['Sex'] == selected_gender)]
+
+
+plot = px.histogram(data_frame=df_plot, x='Age', color='Survived', barmode='group',
+                    title="Age Distribution of Age",template='seaborn',facet_col='Survived')
+
+col1.plotly_chart(plot)
